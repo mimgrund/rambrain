00a5c6e497cf71beda3bf3b9e6cadcd4dbdee9b6
YominCarr <alex.arth@googlemail.com>
2015-03-17 08:53:54 +0100

    Fixed managedPtr test to work with the new exceptions.

--- tests/testManagedPtr.cpp
+++ tests/testManagedPtr.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 #include "managedPtr.h"
+#include "exceptions.h"
 
 TEST(managedPtr, Unit_ParentIDs)
 {
     memoryID parent = managedMemory::defaultManager->parent;
 
-    managedPtr<double> ptr(10);
+    EXPECT_THROW(managedPtr<double> ptr(10), dummyObjectException);
 
     EXPECT_EQ(parent, managedMemory::defaultManager->parent);
 }
