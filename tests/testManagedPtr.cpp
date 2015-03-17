eb665925e49b6fbd5a3c279e05de2a382a1182a5
Maximilian Imgrund <mimgrund@mpifr-bonn.mpg.de>
2015-03-17 09:42:25 +0100

    FIX: Managed Ptr needs an memmanger to operate

--- tests/testManagedPtr.cpp
+++ tests/testManagedPtr.cpp
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
 #include "managedPtr.h"
+#include "cyclicManagedMemory.h"
+#include "managedDummySwap.h"
 
 TEST(managedPtr, Unit_ParentIDs)
 {
+    managedDummySwap swap(0);
+    cyclicManagedMemory mem(&swap,1024);
+
     memoryID parent = managedMemory::defaultManager->parent;
 
     managedPtr<double> ptr(10);
