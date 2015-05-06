72e5da12237ca3c61b1af24dc95bd0e8daf24528
YominCarr <alex.arth@googlemail.com>
2015-05-06 15:28:18 +0200

    Compiler definitions are wrote out to a header file in the lib directory every time the lib is built.

---
+++ scripts/create-definitions-header.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vars=$(echo $1 | tr ";" "\n")
+file="membrainDefinitionsHeader.h"
+wd=$(pwd)
+
+echo "Creating definitions header file $file in $wd"
+
+touch $file
+rm $file
+
+for var in $vars
+do
+	echo "Found definition: $var"
+	
+	if [[ "$var" == __GIT_VERSION=* ]] ;
+	then
+		echo "Skipping"
+	else
+		echo "#define $var" >> $file
+	fi
+done
+
+echo "Done"
