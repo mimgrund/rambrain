1247ddbf8bc434333085fd3c45c9876bf51cdab1
YominCarr <alex.arth@googlemail.com>
2015-05-08 18:53:58 +0200

    FEATURE Added first plot for existing performance test. Need to have a closer look why run 27 and following throw memory exceptions with this configuration. Nevertheless produces first nice plot.

--- scripts/performance-test-config.sh
+++ scripts/performance-test-config.sh
@@ -3,5 +3,13 @@
 TestRepetitions=10
 
 RunMatrixTranspose=1
-MatrixTransposeSize=(1000 2000 3000)
-MatrixTransposeMemory=(10 10 10)
+#MatrixTransposeSize=(1000 2000 3000 4000 5000)
+#MatrixTransposeMemory=(10 10 10 10 10)
+declare -a MatrixTransposeSize
+declare -a MatrixTransposeMemory
+for i in `seq 0 30`;
+do
+  MatrixTransposeSize[$i]=$(echo "" | awk "END {print 10.0 ^ ($i * 4.0 / 30.0 + 1.0)}")
+  MatrixTransposeSize[$i]=${MatrixTransposeSize[$i]%.*}
+  MatrixTransposeMemory[$i]=10
+done
--- scripts/run-performance-tests.sh
+++ scripts/run-performance-tests.sh
@@ -2,11 +2,15 @@
 
 source ./scripts/performance-test-config.sh
 
+touch temp.gnuplot
+touch temp.dat
+
 if [[ $RunMatrixTranspose -eq 1 ]] ;
 then
   countRuns=${#MatrixTransposeSize[@]}
   
   echo "Running ${countRuns} different matrix transposes ${TestRepetitions} times"
+  rm temp.dat
   
   for i in `seq 0 $((countRuns-1))`;
   do
@@ -14,5 +18,34 @@ then
     echo "./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]}"
     
     ./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]}
+    
+    echo -n "${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]} " >> temp.dat
+    while read line
+    do
+      if [[ ! "${line}" == \#* ]] ;
+      then
+	IFS='	' read -a array <<< "$line"
+	
+	arrLength=countRuns=${#array[@]}
+	i=$((arrLength-2))
+	echo -n "${array[$i]} " >> temp.dat
+      fi
+    done < "perftest_MatrixTranspose#${MatrixTransposeSize[$i]}#${MatrixTransposeMemory[$i]}"
+    echo >> temp.dat
+
   done
+  
+  rm temp.gnuplot
+  echo "set terminal png" >> temp.gnuplot
+  echo "set output \"MatrixTranspose.png\"" >> temp.gnuplot
+  echo "set xlabel \"Matrix size per dimension\"" >> temp.gnuplot
+  echo "set ylabel \"Execution time [ms]\"" >> temp.gnuplot
+  echo "set title \"Matrix transpose\"" >> temp.gnuplot
+  echo "set log xy" >> temp.gnuplot
+  echo "plot 'temp.dat' using 1:3 with lines title \"Allocation & Definition\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 1:4 with lines title \"Transposition\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 1:5 with lines title \"Deletion\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 1:(\$3+\$4+\$5) with lines title \"Total\"" >> temp.gnuplot
+  gnuplot temp.gnuplot
+  display MatrixTranspose.png
 fi
