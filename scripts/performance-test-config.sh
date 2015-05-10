b9ae0edba46c37d10a24d8d8f1539199333f2e0c
YominCarr <alex.arth@googlemail.com>
2015-05-10 14:18:11 +0200

    TEST Added another run to check the dependence on main memory. Result seems to be totally independent which I think is strange. Better check everything again before jumping to conclusions.

--- scripts/performance-test-config.sh
+++ scripts/performance-test-config.sh
@@ -2,14 +2,26 @@
 
 TestRepetitions=10
 
-RunMatrixTranspose=1
-#MatrixTransposeSize=(1000 2000 3000 4000 5000)
+
+RunMatrixTranspose1=1
+#MatrixTransposeSize1=(1000 2000 3000 4000 5000)
 #MatrixTransposeMemory=(10 10 10 10 10)
-declare -a MatrixTransposeSize
-declare -a MatrixTransposeMemory
+declare -a MatrixTransposeSize1
+declare -a MatrixTransposeMemory1
 for i in `seq 0 30`;
 do
-  MatrixTransposeSize[$i]=$(echo "" | awk "END {print 10.0 ^ ($i * 4.0 / 30.0 + 1.0)}")
-  MatrixTransposeSize[$i]=${MatrixTransposeSize[$i]%.*}
-  MatrixTransposeMemory[$i]=10
+  MatrixTransposeSize1[$i]=$(echo "" | awk "END {print 10.0 ^ ($i * 4.0 / 30.0 + 1.0)}")
+  MatrixTransposeSize1[$i]=${MatrixTransposeSize1[$i]%.*}
+  MatrixTransposeMemory1[$i]=10
+done
+
+
+RunMatrixTranspose2=1
+declare -a MatrixTransposeSize2
+declare -a MatrixTransposeMemory2
+for i in `seq 0 30`;
+do
+  MatrixTransposeSize2[$i]=8000
+  MatrixTransposeMemory2[$i]=$(echo "" | awk "END {print 10.0 ^ ($i * 3.0 / 30.0 + 1.0)}")
+  MatrixTransposeMemory2[$i]=${MatrixTransposeMemory2[$i]%.*}
 done
--- scripts/run-performance-tests.sh
+++ scripts/run-performance-tests.sh
@@ -5,9 +5,10 @@ source ./scripts/performance-test-config.sh
 touch temp.gnuplot
 touch temp.dat
 
-if [[ $RunMatrixTranspose -eq 1 ]] ;
+
+if [[ $RunMatrixTranspose1 -eq 1 ]] ;
 then
-  countRuns=${#MatrixTransposeSize[@]}
+  countRuns=${#MatrixTransposeSize1[@]}
   
   echo "Running ${countRuns} different matrix transposes ${TestRepetitions} times"
   rm temp.dat
@@ -15,11 +16,11 @@ then
   for i in `seq 0 $((countRuns-1))`;
   do
     echo "Run $((i+1)) ..."
-    echo "./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]}"
+    echo "./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize1[$i]} ${MatrixTransposeMemory1[$i]}"
     
-    ./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]}
+    ./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize1[$i]} ${MatrixTransposeMemory1[$i]}
     
-    echo -n "${MatrixTransposeSize[$i]} ${MatrixTransposeMemory[$i]} " >> temp.dat
+    echo -n "${MatrixTransposeSize1[$i]} ${MatrixTransposeMemory1[$i]} " >> temp.dat
     while read line
     do
       if [[ ! "${line}" == \#* ]] ;
@@ -30,7 +31,7 @@ then
 	i=$((arrLength-2))
 	echo -n "${array[$i]} " >> temp.dat
       fi
-    done < "perftest_MatrixTranspose#${MatrixTransposeSize[$i]}#${MatrixTransposeMemory[$i]}"
+    done < "perftest_MatrixTranspose#${MatrixTransposeSize1[$i]}#${MatrixTransposeMemory1[$i]}"
     echo >> temp.dat
 
   done
@@ -50,6 +51,54 @@ then
   convert -density 300 -resize 1920x MatrixTranspose1.eps -flatten MatrixTranspose1.png
   display MatrixTranspose1.png &
 fi
+
+
+if [[ $RunMatrixTranspose2 -eq 1 ]] ;
+then
+  countRuns=${#MatrixTransposeSize2[@]}
+  
+  echo "Running ${countRuns} different matrix transposes ${TestRepetitions} times"
+  rm temp.dat
+  
+  for i in `seq 0 $((countRuns-1))`;
+  do
+    echo "Run $((i+1)) ..."
+    echo "./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize2[$i]} ${MatrixTransposeMemory2[$i]}"
+    
+    ./bin/membrain-performancetests ${TestRepetitions} MatrixTranspose ${MatrixTransposeSize2[$i]} ${MatrixTransposeMemory2[$i]}
+    
+    echo -n "${MatrixTransposeSize2[$i]} ${MatrixTransposeMemory2[$i]} " >> temp.dat
+    while read line
+    do
+      if [[ ! "${line}" == \#* ]] ;
+      then
+	IFS='	' read -a array <<< "$line"
+	
+	arrLength=countRuns=${#array[@]}
+	i=$((arrLength-2))
+	echo -n "${array[$i]} " >> temp.dat
+      fi
+    done < "perftest_MatrixTranspose#${MatrixTransposeSize2[$i]}#${MatrixTransposeMemory2[$i]}"
+    echo >> temp.dat
+
+  done
+  
+  rm temp.gnuplot
+  echo "set terminal postscript eps enhanced color 'Helvetica,10'" >> temp.gnuplot
+  echo "set output \"MatrixTranspose2.eps\"" >> temp.gnuplot
+  echo "set xlabel \"Matrix rows in main memory\"" >> temp.gnuplot
+  echo "set ylabel \"Execution time [ms]\"" >> temp.gnuplot
+  echo "set title \"Matrix transpose\"" >> temp.gnuplot
+  echo "set log xy" >> temp.gnuplot
+  echo "plot 'temp.dat' using 2:3 with lines title \"Allocation & Definition\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 2:4 with lines title \"Transposition\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 2:5 with lines title \"Deletion\", \\" >> temp.gnuplot
+  echo "'temp.dat' using 2:(\$3+\$4+\$5) with lines title \"Total\"" >> temp.gnuplot
+  gnuplot temp.gnuplot
+  convert -density 300 -resize 1920x MatrixTranspose2.eps -flatten MatrixTranspose2.png
+  display MatrixTranspose2.png &
+fi
+
 
 rm temp.gnuplot
 rm temp.dat
