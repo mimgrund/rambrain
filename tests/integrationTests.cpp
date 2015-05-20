460ef6c37b4619a03ff83a4942abb7529baa8054
YominCarr <alex.arth@googlemail.com>
2015-05-20 11:21:53 +0200

    FIX: Also disabled tests are considered in the integration and unit test split up. Just use something like DISABLED_Unit_MyTest as name.

--- tests/integrationTests.cpp
+++ tests/integrationTests.cpp
@@ -3,6 +3,6 @@
 int main ( int argc, char **argv )
 {
     ::testing::InitGoogleTest ( &argc, argv );
-    ::testing::GTEST_FLAG ( filter ) = "*.Integration_*";
+    ::testing::GTEST_FLAG ( filter ) = "*.Integration_*:*.DISABLED_Integration_*";
     return RUN_ALL_TESTS();
 }
