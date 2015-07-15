e30c4a220f713e8a42117d8cba6f4b8b2bbccfa9
Maximilian Imgrund <mimgrund@mpifr-bonn.mpg.de>
2015-07-15 13:23:44 +0200

    Other test was also fixed by prev. commit

--- src/cyclicManagedMemory.h
+++ src/cyclicManagedMemory.h
@@ -15,11 +15,6 @@ struct cyclicAtime {
 };
 
 
-/**
- * @todo Following segfaults! Run all tests and enter this seed:
- * [ RUN      ] managedFileSwap.Integration_RandomAccess
- * I am running with a seed of 1436882098
- */
 class cyclicManagedMemory : public managedMemory
 {
 public:
