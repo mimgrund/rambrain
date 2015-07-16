28889cb2e36ece59fbf1c1b2481b95b157647acf
YominCarr <alex.arth@googlemail.com>
2015-07-16 11:44:20 +0200

    FEATURE: gtest tests are shuffled now and report the suffling seed for reproducing

--- tests/integrationTests.cpp
+++ tests/integrationTests.cpp
@@ -12,5 +12,7 @@ int main ( int argc, char **argv )
     string customFilter = ::testing::GTEST_FLAG ( filter );
     ::testing::GTEST_FLAG ( filter ) = combineFilter ( generalFilter, customFilter );
 
+    ::testing::GTEST_FLAG ( shuffle ) =  true;
+
     return RUN_ALL_TESTS();
 }
