e5b4d52231a5e49e9a5a4c1375f1ff9f100bebc5
YominCarr <alex.arth@googlemail.com>
2015-07-17 15:12:13 +0200

    DOC: Finished documenting performance test stuff

--- tests/performanceTestClasses.h
+++ tests/performanceTestClasses.h
@@ -201,16 +201,86 @@ protected:
         return parameters[varryParam]->steps;
     }
 
+    /**
+     * @brief Create a string using the current parameter values
+     * @param varryParam Which parameter is stepped through
+     * @param step Which step it is at the moment
+     * @param delimiter A delimiter between the parts
+     * @return The string
+     */
     virtual string getParamsString ( int varryParam, unsigned int step, const string &delimiter = " " );
+    /**
+     * @brief Creates a string for an outfilename using the current parameter values
+     * @param varryParam Which parameter is stepped through
+     * @param step Which step it is at the moment
+     * @return The string
+     */
     virtual string getTestOutfile ( int varryParam, unsigned int step );
+    /**
+     * @brief Read in average timing results from a test and write the important data to a file
+     * @param varryParam Which parameter is stepped through
+     * @param step Which step it is at the moment
+     * @param file The outfile
+     */
     virtual void resultToTempFile ( int varryParam, unsigned int step, ofstream &file );
+    /**
+     * @brief Split a string into a list of substrings
+     * @param in The string
+     * @param delimiter Which delimiter to use for splitting
+     * @return The list
+     */
     virtual vector<string> splitString ( const string &in, char delimiter );
+    /**
+     * @brief Generate a gnuplot script for swapstats output plotting
+     * @param name Name of the output file
+     * @param xlabel Label of the x axis
+     * @param ylabel Label of the y axis
+     * @param title Title of the plot
+     * @param log If plot shall be in logscale
+     * @param paramColumn Column of the temp file to plot
+     * @return The gnuplot script
+     * @see resultToTempFile
+     */
     virtual string generateGnuplotScript ( const string &name, const string &xlabel, const string &ylabel, const string &title, bool log, int paramColumn );
+    /**
+     * @brief Handles the timing informations and produces a plot
+     * @param varryParam Which parameter is stepped through
+     * @param step Which step it is at the moment
+     * @param repetitions The amount of repetitions
+     */
     virtual void handleTimingInfos ( int varryParam, unsigned int step, unsigned int repetitions );
+    /**
+     * @brief Extract timing information from a file
+     * @param in The file
+     * @param start Start time
+     * @param end End time
+     * @return The relevant data
+     */
     virtual vector<vector<string>> getRelevantTimingParts ( ifstream &in, unsigned long long start, unsigned long long end );
+    /**
+     * @brief Writes timing information to a file
+     * @param out The file
+     * @param relevantTimingParts The relevant timing data
+     * @param starttime Start time; Will be changed to first time in data if zero beforehand
+     */
     virtual void timingInfosToFile ( ofstream &out, const vector<vector<string>> &relevantTimingParts, unsigned long long &starttime );
+    /**
+     * @brief Plot timing information
+     * @param gnutemp Outfile for gnuplot script
+     * @param outname Name of resulting file
+     * @param dataFile Name of data file
+     * @param measurements Amount of measurements
+     * @param repetitions Amount of repetitions
+     * @param linesPoints If points shall be added on top of lines for data points
+     */
     virtual void plotTimingInfos ( ofstream &gnutemp, const string &outname, const string &dataFile, unsigned int measurements, unsigned int repetitions, bool linesPoints );
 
+    /**
+     * @brief Create a gnuplot script for plotting test results
+     * @param file Infile name
+     * @param paramColumn Which column to plot as x axis
+     * @return The script
+     */
     virtual string generateMyGnuplotPlotPart ( const string &file, int paramColumn ) = 0;
 
     const char *name;
@@ -221,15 +291,25 @@ protected:
 };
 
 
+/**
+ * @brief Derived performance test classes which take parameter types as template arguments
+ */
 template<typename T, typename... U>
 class performanceTest<T, U...> : public performanceTest<U...>
 {
 
 public:
+    /**
+     * @brief Create a new test object and register it for usage
+     * @param name Name of the test
+     */
     performanceTest ( const char *name ) : performanceTest<U...> ( name ) {
         this->parameters.push_back ( &parameter );
     }
 
+    /**
+     * @brief Cleanup
+     */
     virtual ~performanceTest() {}
 
 protected:
