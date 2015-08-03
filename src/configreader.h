05c152f9f83b38cbffd0a2163bade378bdd76911
Maximilian Imgrund <mimgrund@mpifr-bonn.mpg.de>
2015-08-03 12:59:06 +0200

    FIX: readConfig only if truly necessary

--- src/configreader.h
+++ src/configreader.h
@@ -44,8 +44,7 @@ namespace rambrain
  * Can either keep the boundary fixed, extend it automatically as it needs or start an interactive shell to ask the user for advice
  * @warning Currently not used
  */
-enum class swapPolicy
-{
+enum class swapPolicy {
     fixed,
     autoextendable,
     interactive
@@ -191,6 +190,9 @@ public:
      * @brief Simple getter
      */
     inline configuration &getConfig() {
+        if ( !readSuccess ) {
+            readConfig();
+        }
         return config;
     }
     /**
