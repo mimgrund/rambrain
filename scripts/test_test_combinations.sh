d994195520599d6d12933f1a72747fd9edbe3d39
Maximilian Imgrund <mimgrund@mpifr-bonn.mpg.de>
2015-09-17 16:07:25 +0200

    TEST test_test_combinations.sh now is checking all  sequences

--- scripts/test_test_combinations.sh
+++ scripts/test_test_combinations.sh
@@ -3,7 +3,8 @@
 alltests=$(../bin/rambrain-tests --gtest_list_tests|grep -v "\."|cut -d " " -f3)
 for f in $alltests; do
 	for g in $alltests; do
-		../bin/rambrain-tests --gtest_filter=*$g*:*$f*
+		#repeat because gtest uses same sequence of tests always. then we get all combinations.
+		../bin/rambrain-tests --gtest_repeat=2 --gtest_filter=*$g*:*$f*
 		retval=$?
 		if [ $retval -ne 0 ]; then
 		echo $f $g>>failing
