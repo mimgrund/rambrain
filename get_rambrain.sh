49490834938377f7f61774b97c80a9a0a5ba5556
Maximilian Imgrund <Maximilian.Imgrund@physik.uni-meunchen.de>
2015-11-12 10:40:05 +0100

    FIX: Includepaths and source paths will be set correctly in env

--- get_rambrain.sh
+++ get_rambrain.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo Your hostname is:
+usexp=""
 if hostname|grep cip-ws-; then
         echo "Custom config for CIP PC"
+	usexp="-DUSE_XPRESSIVE=on"
         cat - >~/.rambrain.conf<<EOF
 [default]
 swapfiles=/tmp/rambrain-%d-%d
@@ -16,18 +18,21 @@ echo "Done"
 
 cd rambrain/build
 echo "Building rambrain..."
-cmake ..
+cmake .. $usexp
 make -j 8
 echo "Done"
+cd ..
+sourcedir=$(pwd)/src
+libdir=$(pwd)/lib
 
-cd ../lib
-libdir=$(pwd)
 echo "Library directory is $libdir"
 
 echo "Writing script to load lib path"
-cd ../..
+cd ..
 echo "echo \"Changing library path...\"" > sourceloc.sh
 echo "export LD_LIBRARY_PATH=$LD_LBRARY_PATH:$libdir" >> sourceloc.sh
+echo "export LIBRARY_PATH=$LD_LBRARY_PATH:$libdir" >> sourceloc.sh
+echo "export CPLUS_INCLUDE_PATH=$LD_LBRARY_PATH:$sourcedir" >> sourceloc.sh
 echo "echo \"New libary path should contain rambrain library: $LD_LIBRARY_PATH\"" >> sourceloc.sh
 
 echo "sourceloc.sh written"
