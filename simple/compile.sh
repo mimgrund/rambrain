1e84ac6acddc1a5f6a025895aeaabd9882b037ef
YominCarr <alex.arth@googlemail.com>
2015-11-12 15:08:47 +0100

    Added another run script, little changes.

--- simple/compile.sh
+++ simple/compile.sh
@@ -3,5 +3,5 @@ source ../sourceloc.sh
 
 g++ simple.cpp -g -O0 -o simple
 
-#g++ simple_rambrain.cpp -g -O0 -o simple_rambrain -std=c++11 -lrambrain -lrt
+g++ simple_rambrain.cpp -g -O0 -o simple_rambrain -std=c++11 -lrambrain -lrt
 
--- simple/run.sh
+++ simple/run.sh
@@ -8,13 +8,10 @@ else
 	echo will not run since compilation failed.
 fi
 
-./classes > classical.txt
-./classes_rambrain > rambrain.txt
+./simple > temp.txt
 
 gnuplot - <<EOF
-set terminal png size 1000,400
+set terminal png
 set output "out.png"
-set multiplot layout 1,2
-plot 'classical.txt' matrix w image
-plot 'rambrain.txt' matrix w image
+plot 'temp.txt' matrix w image
 EOF
